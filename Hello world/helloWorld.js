b20c9f237f900506b7a5f116e36822703d60f3e6
Mattia Bressan <mbressan@tecnoscientia.com>
2021-05-13 11:33:15 +0200

    Hello World node

--- Hello world/helloWorld.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World from Node.js!n');
-    console.log('Handled request');
-}).listen(8080, 'localhost');
-console.log('Server running at http://localhost:8080/');
